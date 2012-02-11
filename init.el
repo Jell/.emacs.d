9019856bddebb4037baf42f5d2a7524450e4c811
Jell <jean-louis@icehouse.se>
2012-02-11 15:02:07 +0100

    Make sure the menu bar shows up.

--- init.el
+++ init.el
@@ -1,8 +1,8 @@
 ;; Turn off mouse interface early in startup to avoid momentary display
 ;; You really don't need these; trust me.
-(if (fboundp 'menu-bar-mode) (menu-bar-mode 1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
 
 ;; Key bindings for mac
 (setq mac-option-modifier 'none)
@@ -198,3 +198,5 @@
   (speedbar 1))
 
 (global-auto-revert-mode t)
+
+(menu-bar-mode)
