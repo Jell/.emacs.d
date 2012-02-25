01ecde17becb96d45fe434e5d5891b7145291be8
Jell <jean-louis@icehouse.se>
2012-02-25 14:05:03 +0100

    Added snippet to start multi clojure repl and launch Clojure Script One server.

--- init.el
+++ init.el
@@ -40,11 +40,15 @@
 ;; This is a total hack: we're hardcoding the name of the shell buffer
 (defun shell-send-input (input)
   "Send INPUT into the *shell* buffer and leave it visible."
+  (send-input-to-buffer "*shell*" input))
+
+(defun send-input-to-buffer (buffer input)
+  "Send INPUT to the given BUFFER"
   (save-selected-window
-    (switch-to-buffer-other-window "*shell*")
+    (switch-to-buffer-other-window buffer)
     (goto-char (point-max))
     (insert input)
-    (comint-send-input)))
+    (comint-send-input))))
 
 (defun defun-at-point ()
   "Return the text of the defun at point."
@@ -80,6 +84,24 @@
           '(lambda ()
              (define-key clojure-mode-map (kbd "C-c e") 'shell-eval-last-expression)
              (define-key clojure-mode-map (kbd "C-c x") 'shell-eval-defun)))
+
+(defun start-clj-cljs-repls (path)
+  (interactive (list (read-directory-name "Path to root directory: "))))
+  ;; Start server REPL
+  (shell)
+  (switch-to-prev-buffer)
+  (sleep-for 1)
+  (shell-send-input (format "cd %s" path))
+  (sleep-for 1)
+  (shell-send-input "script/repl")
+  ;; Start cljs REPL
+  (inferior-lisp (format "%s/script/repl" path))
+  (switch-to-prev-buffer))
+
+(defun launch-cljs-one-server ()
+  (interactive)
+  (shell-send-input "(use 'one.sample.dev-server)(run-server)")
+  (send-input-to-buffer "*inferior-lisp*" "(use 'one.sample.dev-server)(cljs-repl)"))
 
 ;; Custom variables
 
