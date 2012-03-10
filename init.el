67dcf5c2ee0c2cab08013d528c096103c4e1f522
Jell <jean-louis@icehouse.se>
2012-03-10 12:43:53 +0100

    Some fun with hooks.

--- init.el
+++ init.el
@@ -24,7 +24,11 @@
 (add-to-list 'exec-path "/usr/local/bin")
 (setq-default ispell-program-name "/usr/local/bin/aspell")
 
+;; Extra load paths
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(add-to-list 'load-path "~/.emacs.d/filetypes")
 
+;; Remove trailing whitespaces
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 ;; Always save even when buffer is not modified
@@ -34,9 +38,6 @@
   (set-buffer-modified-p t)
   (save-buffer))
 (global-set-key (kbd "C-x C-s") 'save-buffer-always)
-
-
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -89,81 +90,12 @@
 ;; Clojure
 (defun turn-on-paredit () (paredit-mode 1))
 (add-hook 'clojure-mode-hook 'turn-on-paredit)
+
+(defun load-extra-clj () (load "clj.el"))
+(add-hook 'clojure-mode-hook 'load-extra-clj)
+
 (add-to-list 'auto-mode-alist '("\.cljs$" . clojure-mode))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; Allow input to be sent to somewhere other than inferior-lisp
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; This is a total hack: we're hardcoding the name of the shell buffer
-(defun shell-send-input (input)
-  "Send INPUT into the *shell* buffer and leave it visible."
-  (send-input-to-buffer "*shell*" input))
-
-(defun send-input-to-buffer (buffer input)
-  "Send INPUT to the given BUFFER"
-  (save-selected-window
-    (switch-to-buffer-other-window buffer)
-    (goto-char (point-max))
-    (insert input)
-    (comint-send-input)))
-
-(defun defun-at-point ()
-  "Return the text of the defun at point."
-  (apply #'buffer-substring-no-properties
-         (region-for-defun-at-point)))
-
-(defun region-for-defun-at-point ()
-  "Return the start and end position of defun at point."
-  (save-excursion
-    (save-match-data
-      (end-of-defun)
-      (let ((end (point)))
-        (beginning-of-defun)
-        (list (point) end)))))
-
-(defun expression-preceding-point ()
-  "Return the expression preceding point as a string."
-  (buffer-substring-no-properties
-   (save-excursion (backward-sexp) (point))
-   (point)))
-
-(defun shell-eval-last-expression ()
-  "Send the expression preceding point to the *shell* buffer."
-  (interactive)
-  (shell-send-input (expression-preceding-point)))
-
-(defun shell-eval-defun ()
-  "Send the current toplevel expression to the *shell* buffer."
-  (interactive)
-  (shell-send-input (defun-at-point)))
-
-(add-hook 'clojure-mode-hook
-          '(progn
-             (define-key clojure-mode-map (kbd "C-c e") 'shell-eval-last-expression)
-             (define-key clojure-mode-map (kbd "C-c x") 'shell-eval-defun)))
-
-(defun start-clj-cljs-repls (path)
-  "Starts a Clojure and a ClojureScript REPL"
-  (interactive (list (read-directory-name "Path to root directory: ")))
-  ;; Start server REPL
-  (shell)
-  (switch-to-prev-buffer)
-  (sleep-for 1)
-  (shell-send-input (format "cd %s" path))
-  (sleep-for 1)
-  (shell-send-input "script/repl")
-  ;; Start cljs REPL
-  (inferior-lisp (format "%s/script/repl" path))
-  (switch-to-prev-buffer))
-
-(defun launch-cljs-one-server ()
-  "Start a ClojureScript One interactive REPL"
-  (interactive)
-  (shell-send-input "(use 'one.sample.dev-server)(run-server)")
-  (send-input-to-buffer "*inferior-lisp*" "(use 'one.sample.dev-server)(cljs-repl)"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -171,7 +103,6 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
 ;; Install hooks, called only after the package is installed (so we
 ;; can install them asynchronously. Pretty cool stuff.
