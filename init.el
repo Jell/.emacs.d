aec04f40aee13a02930ff38cbaaacbf5f3ff6753
Jell <jean-louis@icehouse.se>
2012-03-26 22:30:03 +0200

    Hack to fix emacs' $PATH

--- init.el
+++ init.el
@@ -66,6 +66,13 @@
 (setq stack-trace-on-error t)
 (setq imenu-auto-rescan t)
 
+(defun set-exec-path-from-shell-PATH ()
+  (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo $PATH'")))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(if window-system (set-exec-path-from-shell-PATH))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;; ELPA packages
