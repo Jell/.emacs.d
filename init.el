8f3dcda1545b3b3f84492826a0a6bf56874e01b6
Jell <jean-louis@jawaninja.com>
2012-05-12 18:02:53 +0200

    Fixed yasnippet

--- init.el
+++ init.el
@@ -275,29 +275,41 @@
                :url "http://github.com/yoshiki/yaml-mode.git"
                :features yaml-mode
                :post-init (lambda () (yaml-mode-hook)))
-        ;(:name yasnippet
-        ;       :website "https://github.com/capitaomorte/yasnippet.git"
-        ;       :description "YASnippet is a template system for Emacs."
-        ;       :type github
-        ;       :pkgname "capitaomorte/yasnippet"
-        ;       :features "yasnippet"
-        ;       ;; Set up the default snippets directory
-        ;       ;;
-        ;       ;; Principle: don't override any user settings for
-        ;       ;; yas/snippet-dirs, whether those were made with setq or
-        ;       ;; customize. If the user doesn't want the default snippets,
-        ;       ;; she shouldn't get them!
-        ;       :pre-init (unless (or (boundp 'yas/snippet-dirs)
-        ;                             (get 'yas/snippet-dirs 'customized-value))
-        ;                   (setq yas/snippet-dirs
-        ;                         (list (concat el-get-dir
-        ;                                       (file-name-as-directory "yasnippet")
-        ;                                       "snippets"))))
-        ;       :post-init (yasnippet-hook)
-        ;       ;; byte-compile load vc-svn and that fails
-        ;       ;; see https://github.com/dimitri/el-get/issues/200
-        ;       :compile nil
-        ;       :submodule nil)
+        (:name yasnippet
+               :website "http://code.google.com/p/yasnippet/"
+               :description "YASnippet is a template system for Emacs."
+               :type git
+               :url "https://github.com/capitaomorte/yasnippet.git"
+               :features "yasnippet"
+               :prepare (lambda ()
+                          ;; Set up the default snippets directory
+                          ;;
+                          ;; Principle: don't override any user settings
+                          ;; for yas/snippet-dirs, whether those were made
+                          ;; with setq or customize.  If the user doesn't
+                          ;; want the default snippets, she shouldn't get
+                          ;; them!
+                          (unless (or (boundp 'yas/snippet-dirs) (get 'yas/snippet-dirs 'customized-value))
+                            (setq yas/snippet-dirs
+                                  (list (concat el-get-dir (file-name-as-directory "yasnippet") "snippets")))))
+               :post-init (lambda ()
+                            ;; Trick customize into believing the standard
+                            ;; value includes the default snippets.
+                            ;; yasnippet would probably do this itself,
+                            ;; except that it doesn't include an
+                            ;; installation procedure that sets up the
+                            ;; snippets directory, and thus doesn't know
+                            ;; where those snippets will be installed.  See
+                            ;; http://code.google.com/p/yasnippet/issues/detail?id=179
+                            (put 'yas/snippet-dirs 'standard-value
+                                 ;; as cus-edit.el specifies, "a cons-cell
+                                 ;; whose car evaluates to the standard
+                                 ;; value"
+                                 (list (list 'quote
+                                             (list (concat el-get-dir (file-name-as-directory "yasnippet") "snippets"))))))
+               ;; byte-compile load vc-svn and that fails
+               ;; see https://github.com/dimitri/el-get/issues/200
+               :compile nil)
         (:name auto-complete
                :description "The most intelligent auto-completion extension."
                :type git
@@ -347,14 +359,14 @@
   (el-get 'sync my-packages))
 
 (if (require 'el-get nil t)
-  (sync-packages)
+    (sync-packages)
   (url-retrieve
-    "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
-    (lambda (s)
-      (end-of-buffer)
-      (eval-print-last-sexp)
-      (setq el-get-verbose t)
-      (sync-packages))))
+   "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
+   (lambda (s)
+     (end-of-buffer)
+     (eval-print-last-sexp)
+     (setq el-get-verbose t)
+     (sync-packages))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
