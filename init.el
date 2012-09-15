1ae6fa1f45b0b66d7f3c7ff3ed52cd672e3d6a3d
Jell <jean-louis@jawaninja.com>
2012-09-15 13:52:35 +0200

    Piff.

--- init.el
+++ init.el
@@ -6,57 +6,6 @@
 ;; Key bindings for mac
 (setq mac-option-modifier 'none)
 (setq mac-command-modifier 'meta)
-;; Custom mode-line
-(setq-default mode-line-format
-              (list
-               ;; the buffer name; the file name as a tool tip
-               '(:eval (propertize " %b " 'face 'font-lock-keyword-face
-                                   'help-echo (buffer-file-name)))
-
-               ;; line and column
-               (propertize " L%01l " 'face 'font-lock-type-face
-                           'help-echo '(format-mode-line "Line: %01l, Column: %01c"))
-
-               '(:eval (when evil-mode evil-mode-line-tag))
-
-               ;; the current major mode for the buffer.
-               '(:eval (propertize (concat " "
-                                           (format-mode-line mode-name)
-                                           " ")
-                                   'face 'font-lock-string-face
-                                   'help-echo (concat "Minor modes: ["
-                                                      (format-mode-line minor-mode-alist)
-                                                      " ]")))
-
-               '(:eval (when nyan-mode
-                         (concat
-                          " "
-                          (propertize (format-mode-line (list (nyan-create)))
-                                      'help-echo "nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan..."))))
-
-               ;; add the time, with the date and the emacs uptime in the tooltip
-               '(:eval (propertize (format-time-string " %H:%M ")
-                                   'help-echo
-                                   (concat (format-time-string "%c; ")
-                                           (emacs-uptime "Uptime:%hh"))))
-
-               '(:eval (propertize (if overwrite-mode " Ovr " " Ins ")
-                                   'face 'font-lock-preprocessor-face
-                                   'help-echo (concat "Buffer is in "
-                                                      (if overwrite-mode "overwrite" "insert")
-                                                      " mode")))
-
-               ;; is this buffer read-only?
-               '(:eval (when buffer-read-only
-                         (propertize " RO "
-                                     'face 'font-lock-type-face
-                                     'help-echo "Buffer is read-only")))
-
-               ;; was this buffer modified since the last save?
-               '(:eval (when (buffer-modified-p)
-                         (propertize " Mod "
-                                     'face 'font-lock-warning-face
-                                     'help-echo "Buffer has been modified")))))
 
 ;; Soft word wrap
 (global-visual-line-mode)
@@ -107,6 +56,7 @@
         try-expand-whole-kill))
 
 ;; Extra load paths
+(add-to-list 'load-path "~/.emacs.d")
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 (add-to-list 'load-path "~/.emacs.d/filetypes")
 (add-to-list 'load-path "~/.emacs.d/other")
@@ -486,6 +436,9 @@
 ;; Stuff to trigger last
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Custom mode-line
+(load "setup-mode-line.el")
 
 ;; Do not *ding* on scroll and other stuff.
 (defun my-bell-function ()
