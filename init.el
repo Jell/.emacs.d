bba84fe9cc913bf02137f1471a567caf822b99cc
Jell <jean-louis@jawaninja.com>
2012-12-23 13:01:54 +0100

    Start magit in fullscreen.

--- init.el
+++ init.el
@@ -169,6 +169,21 @@
 (require 'undo-tree)
 (require 'magit)
 
+;; full screen magit-status
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(defun magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
+(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
+
+
 ;; Erlang
 (autoload 'erlang-mode "erlang" nil t)
 (add-to-list 'auto-mode-alist '("\\.erl$" . erlang-mode))
