99c51e925e9e79167e04019490d31ac792fdb6f8
Jell <jean-louis@jawaninja.com>
2017-11-05 09:08:32 +0100

    Fix markdown

--- el-get-user/init/init-markdown-mode.el
+++ el-get-user/init/init-markdown-mode.el
@@ -1,2 +1,6 @@
+(defun load-markdown-mode ()
+  (require 'markdown-mode)
+  (markdown-mode))
+
 (add-to-list 'auto-mode-alist
-             '("\\.md\\'" . markdown-mode))
+             '("\\.md\\'" . load-markdown-mode))
